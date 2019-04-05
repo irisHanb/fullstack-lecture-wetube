89b53b72aea21a88dc86dd32c2623c0e0d88f25e
mz-hanb <hanb@mz.co.kr>
2019-04-05 18:21:21 +0900

    add pug

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.render('join');
+export const login = (req, res) => res.send('login');
+export const logout = (req, res) => res.send('logout');
+export const users = (req, res) => res.send('users');
+export const uerDetail = (req, res) => res.send('uerDetail');
+export const editProfile = (req, res) => res.send('editProfile');
+export const changePassword = (req, res) => res.send('changePassword');
