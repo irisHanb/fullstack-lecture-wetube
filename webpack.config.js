f506f02fdc09ebdff9ded997f92801d107334082
mz-hanb <hanb@mz.co.kr>
2019-04-16 17:50:56 +0900

    styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const ExtractCSS = require('extract-text-webpack-plugin');
 
-const MODE = process.env.WEBPACK_ENV;
+// const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js');
 const OUTPUT_DIR = path.join(__dirname, 'static');
 
 const config = {
   entry: ['@babel/polyfill', ENTRY_FILE],
-  mode: MODE,
+  // mode: MODE,
   module: {
     rules: [
       {
